79db3c77f1001e628dfaefb9ecc52841b936c647
RpPrime <Nikitamatveev007@gmail.com>
2023-06-21 11:07:01 +0300

    Задача 23 готова

---
+++ Sem3.2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i <= number; i++)
+{
+    int itog = Convert.ToInt32(Math.Pow(i, 3));
+    Console.WriteLine(itog);
+}
