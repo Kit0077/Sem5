f517493acefee1042b6c30ccc36ca73b26d88f0b
RpPrime <Nikitamatveev007@gmail.com>
2023-06-21 11:33:48 +0300

    Задание 19 готово

---
+++ Sem3.3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string NumberToString = Convert.ToString(number);
+int lenght = NumberToString.Length;
+
+if (lenght == 5)
+{
+    if (NumberToString[0] == NumberToString[4] && NumberToString[1] == NumberToString[3])
+    {
+        Console.WriteLine("Ваше число является палиндромом");
+    }
+    else Console.WriteLine("Ваше число не палиндром");
+} 
+else Console.WriteLine("Выше число не пятизначное");
