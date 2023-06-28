dc84320c08a8f2b066d12628c2acbe2cd6742e10
RpPrime <Nikitamatveev007@gmail.com>
2023-06-28 17:01:51 +0300

    адание 27 готово

---
+++ Sem4.1/Program.cs
@@ -0,0 +1,17 @@
+﻿int MegaSum(int Num)
+{
+   int Num2 = 0;
+   int Num3 = 0;
+    while (Num > 0)
+    {
+        Num2 = Num % 10;
+        Num3 = Num2 + Num3;
+        Num = Num / 10;
+        
+    }
+    return Num3;
+}
+
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе {number} равна {MegaSum(number)}");
